18c25dadaec021086d2742fc2fd1365101fc245f
Leonardo Henrique Guimarães <45611814+l1n3rd4@users.noreply.github.com>
2020-05-08 16:37:07 -0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,29 @@ public class Main {
     public static void main(String[] args) {
         Hibernate.getSessionFactory();
         Category category = new Category();
+        
+                post("/users", (request, response) -> {
+            
+        });
+        
+        get("/users", (request, response) -> {
+            
+        });
+        
+        get("/users/:id", (request, response) -> {
+            
+        });
+        
+        put("/users/:id", (request, response) -> {
+            
+        });
+        
+        delete("/users/:id", (request, response) -> {
+            
+        });
+        
+        options("/users/:id", (request, response) -> {
+            
+        });
     }
 }
